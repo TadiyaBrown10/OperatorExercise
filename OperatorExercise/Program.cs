d643643fa7ff7a4c854db2c98ae3824e591ae319
Tadiya Brown <tadiyabrown2000@gmail.com>
2022-08-01 17:07:00 -0400

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+
+            var b = 4;
+
+            int div = a / b;
+            int mod = a % b;
+
+
+            if (a == 17 && b == 4)
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+        }
+    }
+}
+
